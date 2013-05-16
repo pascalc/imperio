400199b8abc46f309b51ec050f79474d01e8b0b0
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2013-05-16 16:25:13 +0200

    Removed unnecessary wrapper

--- resources/public/js/websocket.js
+++ resources/public/js/websocket.js
@@ -1,29 +1,28 @@
-$(document).ready(function() {
-  ws = new WebSocket("ws://10.0.0.11:4000/socket");
+ws = new WebSocket("ws://10.0.0.11:4000/socket");
+console.log("readyState:", ws.readyState);
+
+ws.onopen = function()
+{
+  // Web Socket is connected, send data using send()
+  console.log("Socket opened!");
   console.log("readyState:", ws.readyState);
+};
 
-  ws.onopen = function()
-  {
-    // Web Socket is connected, send data using send()
-    console.log("Socket opened!");
-    console.log("readyState:", ws.readyState);
-  };
+ws.onclose = function()
+{ 
+  // websocket is closed.
+  console.log("Socket closed!");
+  console.log("readyState:", ws.readyState);
+};
 
-  ws.onclose = function()
-  { 
-    // websocket is closed.
-    console.log("Socket closed!");
-    console.log("readyState:", ws.readyState);
-  };
+send = function(msg) {
+  ws.send(msg);
+}
 
-  send = function(msg) {
-    ws.send(msg);
-  }
+command = function(cmd, args) {
+  send(JSON.stringify({
+    'command': cmd,
+    'args' : args,
+  }));
+}  
 
-  command = function(cmd, args) {
-    send(JSON.stringify({
-      'command': cmd,
-      'args' : args,
-    }));
-  }  
-});
