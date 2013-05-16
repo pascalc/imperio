1988fa724269c6c3800180edd4be035a107e98ac
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2013-05-16 13:56:36 +0200

    Serve static files with Compojure

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.1"]
+                 [compojure "1.1.5"]
                  [cheshire "5.1.1"]])
--- src/imperio/server.clj
+++ src/imperio/server.clj
@@ -1,12 +1,18 @@
 (ns imperio.server
-  (:use [org.httpkit.server])
-  (:require [cheshire.core :as json]
+  (:use [org.httpkit.server]
+        [compojure.handler :only [site]]
+        [compojure.core :only [defroutes GET]])
+  (:require [compojure.route :as route]
+            [ring.util.response :as resp]
+            [cheshire.core :as json]
             [imperio.protocol :as imperio]))
 
 (defn parse [s]
   (-> s
       json/parse-string
       clojure.walk/keywordize-keys))
+
+;; Handlers
 
 (defn ws-handler [request]
   (with-channel request ch
@@ -16,11 +22,23 @@
                   (println data)
                   (imperio/execute! (parse data))))))
 
+;; Routes
+
+(defroutes routes
+  (GET "/" []
+       (resp/file-response "index.html" {:root "resources/public"}))
+  (GET "/socket" [:as request]
+       (ws-handler request))
+  (route/resources "/"))
+
+
+;; Server
+
 (defonce server (atom nil))
 
 (defn start-server! []
   (reset! server
-          (run-server #'ws-handler {:port 1410})))
+          (run-server (site #'routes) {:port 1410})))
 
 (defn stop-server! []
   (@server))
